ea2599f6f998b7066f23cd3486412799b9b74d06
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2020-12-03 16:54:24 +0800

    用vite开发react开发环境

---
+++ React-vite/vite.config.js
@@ -0,0 +1,19 @@
+// 项目初始化
+// // @ts-check
+// import reactPlugin from 'vite-plugin-react'
+
+// /**
+//  * @type { import('vite').UserConfig }
+//  */
+// const config = {
+//   jsx: 'react',
+//   plugins: [reactPlugin]
+// }
+
+// export default config
+
+//debug(github)
+module.exports = {
+  jsx: 'react',
+  plugins: [require('vite-plugin-react')]
+}
