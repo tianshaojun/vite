b2242f72505a638681294551956612f3671975c5
田少军 <tianshaojun@tianshaojundeMacBook-Pro.local>
2020-12-06 20:07:39 +0800

    20201206

--- honglangman/src/App.vue
+++ honglangman/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+const jspang: string = "jspang.com";
+import "./assets/app.css";
+import data from './assets/jspang.json';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.log(jspang);
+    console.log(data.name);
+    console.log(data.website);
+  },
+};
 </script>
